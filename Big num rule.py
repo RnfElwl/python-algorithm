af949f2e510a94be8e915ffa109eefeca1ce226c
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2021-09-08 22:05:44 +0900

    Update Big num rule.py

--- Big num rule.py
+++ Big num rule.py
@@ -1,20 +1,23 @@
+
 n, m, k = map(int, input().split())
 
-data = list(map(int, input().split()))
+data = list(map(int, input().split()))// n 번 입력받고
 
 data.sort()
 
-first = data[n-1]
-second = data[n-2]
+first = data[n-1]//가장큰수
+second = data[n-2]//두번째로 큰 수
 count, sum = 0, 0
-for i in range(m):
+for i in range(m): //m번 더해줌
     count += 1
-    if count%(k+1) == 0:
+    if count%(k+1) == 0://근데 가장큰수가 k번 넘으면 안됨
         sum += second
 
     else:
         sum += first
 
-    print(sum)
+ 
 
-print(sum)
+print(sum)//큰수의 법칙에 맞는 값 출력
+// O(m)의 복잡도가 아닌가?싶다
+// 일단 정확한 답이 나오는지 잘 모르겠지만 아마도 맞을것같다
