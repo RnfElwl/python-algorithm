cfd0018b74c497606c65918a79ed0ca339a39e6e
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2021-09-26 17:32:06 +0900

    Update croatia alphabet.py

--- croatia alphabet.py
+++ croatia alphabet.py
@@ -5,5 +5,6 @@ croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 
 for i in croatia:
     word = word.replace(i, "*")
-
+#길이를 아예 1로 줄이니까 그냥 크로아티아랑 알파벳을 합친 갯수가 같아 지네;;
 print(len(word))
+#난 병따개다
